d3861fd8586f3570715fd4187a3820c157c5b6bc
BenABaron <benabaron@gmail.com>
2020-12-17 20:25:07 -0600

    git test

--- week0/hello.js
+++ week0/hello.js
@@ -1,1 +1,1 @@
-console.log("Hello Poopy!");
+console.log("Hello Class!");
---
+++ week0/variables.js
@@ -0,0 +1,58 @@
+// let printHello = function() {
+//     console.log("Hello")
+// }
+
+// let add = function(input1, input2) {
+//     //logic, what the function does
+//     console.log("input 1 = ", input1)
+//     console.log("input 2 = ", input2)
+//     let sum = input1 + input2
+//     return sum
+// }
+
+// let x = 12
+// let a = 18
+
+// add(x, a)
+
+let subtract = function(input1, input2){
+    console.log("subtraction")
+    console.log("input 1 = ", input1)
+    console.log("input 2 = ", input2)
+    let diff = input1 - input2
+    console.log(diff)
+    //take in input1 and input2 and return the difference
+}
+
+let multiply = function(input1, input2){
+    console.log("multiplication")
+    console.log("input 1 = ", input1)
+    console.log("input 2 = ", input2)
+    let product = input1 * input2
+    console.log(product)
+    //same but product
+}
+
+let divide = function(input1, input2){
+    console.log("division")
+    console.log("input 1 = ", input1)
+    console.log("input 2 = ", input2)
+    let div = input1 / input2
+    console.log(div)
+    //same but quotient
+}
+
+subtract(24, 13)
+multiply(5, 7)
+divide(40, 8)
+
+let f1 = function(input) {
+    console.log(input)
+}
+
+function f2(input) {
+    console.log(input)
+}
+let f3 = (input) => {
+    console.log(input)
+}
